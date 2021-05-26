1d963c1ad0fae12bbd50850e6e1050ad1682d922
shimizacken <shimizacken@gmail.com>
2021-05-26 22:32:13 +0200

    refactor read input

--- src/core/readLog.ts
+++ src/core/readLog.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as readline from "readline";
 import { ANSIFontStyling } from "../enums.js";
 import type { LogEntry } from "../types";
 import { toTime } from "../utils/utils.js";
@@ -25,186 +26,202 @@ export const readLog = (logFile?: string): Promise<LogEntry[]> => {
 };
 
 export const main = () => {
-  const stdin = process.openStdin();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  console.log("Enter log file path: ");
+  const state = {
+    path: "",
+  };
 
-  stdin.addListener("data", function (input) {
-    const value = input.toString().trim();
+  rl.question(`Enter log file path: `, (input) => {
+    state.path = input;
 
-    const results = readLog(`${value}`);
+    rl.question(`Select preset: Mute or enter for None`, (country) => {
+      const value = state.path?.toString().trim();
 
-    results.then((result) => {
-      const mediaEvents = {};
+      const results = readLog(`${value}`);
 
-      const topicId = result?.find((log) => log?.payload?.type === "member")
-        ?.payload?.topic;
+      results.then((result) => {
+        const mediaEvents = {};
 
-      const topicLogs = result
-        .filter((message) => {
-          if (message.event?.topic && message.event?.topic !== topicId) {
-            return undefined;
-          }
+        const topicId = result?.find((log) => log?.payload?.type === "member")
+          ?.payload?.topic;
 
-          return message;
-        })
-        .filter(Boolean);
+        const topicLogs = result
+          .filter((message) => {
+            if (message.event?.topic && message.event?.topic !== topicId) {
+              return undefined;
+            }
 
-      console.log(
-        `${ANSIFontStyling.FgYellow}${ANSIFontStyling.Underscore}Log summary:${ANSIFontStyling.Reset}\n`
-      );
+            return message;
+          })
+          .filter(Boolean);
 
-      console.log(`${ANSIFontStyling.FgCyan} topic: ${topicId}\n`);
-
-      const names = topicLogs
-        .map((obj) => {
-          if (obj.name) {
-            return obj.name;
-          }
-        })
-        .filter(Boolean);
-
-      console.log(
-        ANSIFontStyling.FgYellow,
-        `${ANSIFontStyling.Underscore}Log names:${ANSIFontStyling.Reset}`
-      );
-
-      new Set(names).forEach((name) =>
-        console.log(`${ANSIFontStyling.FgCyan} ${name}`)
-      );
-
-      const lighthouseEvents = topicLogs
-        .map((obj) => {
-          if (obj.event?.type) {
-            return obj.event.type;
-          }
-        })
-        .filter(Boolean);
-
-      console.log(ANSIFontStyling.Reset);
-
-      console.log(
-        ANSIFontStyling.FgYellow,
-        `${ANSIFontStyling.Underscore}lighthouse events:${ANSIFontStyling.Reset}`
-      );
-
-      console.log(
-        ANSIFontStyling.FgCyan,
-        Array.from(new Set(lighthouseEvents)).join(", ")
-      );
-
-      console.log(ANSIFontStyling.Reset);
-
-      console.log(
-        ANSIFontStyling.FgYellow,
-        `${ANSIFontStyling.Underscore}Log:${ANSIFontStyling.Reset}\n`
-      );
-
-      topicLogs.forEach((log) => {
         console.log(
-          ANSIFontStyling.FgGreen,
-          `${log.time} | ${toTime(log.time)}`
+          `${ANSIFontStyling.FgYellow}${ANSIFontStyling.Underscore}Log summary:${ANSIFontStyling.Reset}\n`
         );
-        console.log(ANSIFontStyling.FgCyan, `name: ${log.name}`);
 
-        log.msg
-          ? console.log(ANSIFontStyling.FgYellow, `message: ${log.msg}`)
-          : "";
+        console.log(`${ANSIFontStyling.FgCyan} topic: ${topicId}\n`);
 
-        log.isTrusted
-          ? console.log(ANSIFontStyling.Reset, `isTrusted: ${log.isTrusted}`)
-          : "";
+        const names = topicLogs
+          .map((obj) => {
+            if (obj.name) {
+              return obj.name;
+            }
+          })
+          .filter(Boolean);
 
-        if (log.name === "router") {
-          console.log(
-            "\x1b[0m",
-            `title: ${log.url?.state?.title}, url: ${log.url?.path}`
-          );
-        }
+        console.log(
+          ANSIFontStyling.FgYellow,
+          `${ANSIFontStyling.Underscore}Log names:${ANSIFontStyling.Reset}`
+        );
 
-        log.mute !== undefined
-          ? console.log(ANSIFontStyling.FgCyan, `mute: ${log.mute}`)
-          : "";
+        new Set(names).forEach((name) =>
+          console.log(`${ANSIFontStyling.FgCyan} ${name}`)
+        );
 
-        if (log?.name === "lighthouse") {
-        }
+        const lighthouseEvents = topicLogs
+          .map((obj) => {
+            if (obj.event?.type) {
+              return obj.event.type;
+            }
+          })
+          .filter(Boolean);
 
-        const sentMediaEvent =
-          log?.name === "lighthouse" && log.payload ? log.payload : undefined;
+        console.log(ANSIFontStyling.Reset);
 
-        if (sentMediaEvent) {
-          console.log(ANSIFontStyling.FgYellow, `type: ${sentMediaEvent.type}`);
-          sentMediaEvent.at
-            ? console.log(ANSIFontStyling.FgGreen, `at: ${sentMediaEvent.at}`)
-            : "";
+        console.log(
+          ANSIFontStyling.FgYellow,
+          `${ANSIFontStyling.Underscore}lighthouse events:${ANSIFontStyling.Reset}`
+        );
+
+        console.log(
+          ANSIFontStyling.FgCyan,
+          Array.from(new Set(lighthouseEvents)).join(", ")
+        );
+
+        console.log(ANSIFontStyling.Reset);
+
+        console.log(
+          ANSIFontStyling.FgYellow,
+          `${ANSIFontStyling.Underscore}Log:${ANSIFontStyling.Reset}\n`
+        );
+
+        topicLogs.forEach((log) => {
           console.log(
             ANSIFontStyling.FgGreen,
-            `⬆ ${ANSIFontStyling.FgWhite}sent`
+            `${log.time} | ${toTime(log.time)}`
           );
-        }
+          console.log(ANSIFontStyling.FgCyan, `name: ${log.name}`);
 
-        const receiveLighthouseEvent =
-          log?.name === "lighthouse" && log.event ? log.event : undefined;
-
-        if (receiveLighthouseEvent) {
-          console.log(
-            ANSIFontStyling.FgYellow,
-            `type: ${receiveLighthouseEvent.type}`
-          );
-          console.log(
-            ANSIFontStyling.FgRed,
-            `⬇ ${ANSIFontStyling.FgWhite}received`
-          );
-          receiveLighthouseEvent.at
-            ? console.log(
-                ANSIFontStyling.FgGreen,
-                `at: ${receiveLighthouseEvent.at}`
-              )
+          log.msg
+            ? console.log(ANSIFontStyling.FgYellow, `message: ${log.msg}`)
             : "";
 
-          if (receiveLighthouseEvent?.type === "media") {
+          log.isTrusted
+            ? console.log(ANSIFontStyling.Reset, `isTrusted: ${log.isTrusted}`)
+            : "";
+
+          if (log.name === "router") {
             console.log(
-              ANSIFontStyling.FgCyan,
-              `audio: ${receiveLighthouseEvent.audio}`
+              "\x1b[0m",
+              `title: ${log.url?.state?.title}, url: ${log.url?.path}`
             );
           }
 
-          if (receiveLighthouseEvent?.type === "member") {
+          log.mute !== undefined
+            ? console.log(ANSIFontStyling.FgCyan, `mute: ${log.mute}`)
+            : "";
+
+          if (log?.name === "lighthouse") {
+          }
+
+          const sentMediaEvent =
+            log?.name === "lighthouse" && log.payload ? log.payload : undefined;
+
+          if (sentMediaEvent) {
             console.log(
-              ANSIFontStyling.FgCyan,
-              `can: ${JSON.stringify(receiveLighthouseEvent.can)}`
+              ANSIFontStyling.FgYellow,
+              `type: ${sentMediaEvent.type}`
+            );
+            sentMediaEvent.at
+              ? console.log(ANSIFontStyling.FgGreen, `at: ${sentMediaEvent.at}`)
+              : "";
+            console.log(
+              ANSIFontStyling.FgGreen,
+              `⬆ ${ANSIFontStyling.FgWhite}sent`
             );
           }
 
-          if (
-            receiveLighthouseEvent?.at &&
-            mediaEvents[receiveLighthouseEvent.callId]?.at
-          ) {
-            if (
-              new Date(receiveLighthouseEvent.at).valueOf() <
-              new Date(mediaEvents[receiveLighthouseEvent.callId].at).valueOf()
-            ) {
+          const receiveLighthouseEvent =
+            log?.name === "lighthouse" && log.event ? log.event : undefined;
+
+          if (receiveLighthouseEvent) {
+            console.log(
+              ANSIFontStyling.FgYellow,
+              `type: ${receiveLighthouseEvent.type}`
+            );
+            console.log(
+              ANSIFontStyling.FgRed,
+              `⬇ ${ANSIFontStyling.FgWhite}received`
+            );
+            receiveLighthouseEvent.at
+              ? console.log(
+                  ANSIFontStyling.FgGreen,
+                  `at: ${receiveLighthouseEvent.at}`
+                )
+              : "";
+
+            if (receiveLighthouseEvent?.type === "media") {
               console.log(
-                ANSIFontStyling.FgRed,
-                `\t${ANSIFontStyling.Underscore}received out of order!${ANSIFontStyling.Reset} 🤪\n`,
-                `\t${ANSIFontStyling.FgWhite}previous media event at ${
-                  ANSIFontStyling.FgGreen
-                }${mediaEvents[receiveLighthouseEvent.callId]?.at} ${
-                  ANSIFontStyling.FgWhite
-                }and the next at ${ANSIFontStyling.FgGreen}${
-                  receiveLighthouseEvent?.at
-                }`
+                ANSIFontStyling.FgCyan,
+                `audio: ${receiveLighthouseEvent.audio}`
               );
+            }
+
+            if (receiveLighthouseEvent?.type === "member") {
+              console.log(
+                ANSIFontStyling.FgCyan,
+                `can: ${JSON.stringify(receiveLighthouseEvent.can)}`
+              );
+            }
+
+            if (
+              receiveLighthouseEvent?.at &&
+              mediaEvents[receiveLighthouseEvent.callId]?.at
+            ) {
+              if (
+                new Date(receiveLighthouseEvent.at).valueOf() <
+                new Date(
+                  mediaEvents[receiveLighthouseEvent.callId].at
+                ).valueOf()
+              ) {
+                console.log(
+                  ANSIFontStyling.FgRed,
+                  `\t${ANSIFontStyling.Underscore}received out of order!${ANSIFontStyling.Reset} 🤪\n`,
+                  `\t${ANSIFontStyling.FgWhite}previous media event at ${
+                    ANSIFontStyling.FgGreen
+                  }${mediaEvents[receiveLighthouseEvent.callId]?.at} ${
+                    ANSIFontStyling.FgWhite
+                  }and the next at ${ANSIFontStyling.FgGreen}${
+                    receiveLighthouseEvent?.at
+                  }`
+                );
+              }
+            }
+
+            if (receiveLighthouseEvent.type === "media") {
+              mediaEvents[receiveLighthouseEvent.callId] =
+                receiveLighthouseEvent;
             }
           }
 
-          if (receiveLighthouseEvent.type === "media") {
-            mediaEvents[receiveLighthouseEvent.callId] = receiveLighthouseEvent;
-          }
-        }
-
-        console.log("");
+          console.log("");
+        });
       });
+      // close the stream
     });
   });
 };
