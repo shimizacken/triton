02e5614bdc2cc6590672d4b2d5d305fd8cb4851f
shimizacken <shimizacken@gmail.com>
2021-05-26 14:43:49 +0200

    events and names added

--- src/core/readLog.js
+++ src/core/readLog.js
@@ -62,52 +62,80 @@ stdin.addListener("data", function (input) {
 
   const results = readLog("src/logs/pexip-2021-05-25T06-23-49.542Z.log"); //readLog(`${value}`);
 
-  results.then((r) => {
-    r.filter((message) => {
-      if (
-        message.event?.topic &&
-        message.event?.topic !== "6b3dd8e4-9c11-43d9-89dd-bb592b92f093"
-      ) {
-        return undefined;
+  results.then((result) => {
+    const topic = result
+      .filter((message) => {
+        if (
+          message.event?.topic &&
+          message.event?.topic !== "6b3dd8e4-9c11-43d9-89dd-bb592b92f093"
+        ) {
+          return undefined;
+        }
+
+        return message;
+      })
+      .filter(Boolean);
+
+    console.log(Colors.Reset, "");
+
+    const names = topic
+      .map((obj) => {
+        if (obj.name) {
+          return obj.name;
+        }
+      })
+      .filter(Boolean);
+
+    console.log(Colors.FgCyan, "names", [...new Set(names)]);
+
+    const lighthouseEvents = topic
+      .map((obj) => {
+        if (obj.event?.type) {
+          return obj.event.type;
+        }
+      })
+      .filter(Boolean);
+
+    console.log(Colors.Reset, "");
+
+    console.log(Colors.FgCyan, "events", [...new Set(lighthouseEvents)]);
+
+    console.log(Colors.Reset, "");
+
+    topic.forEach((log) => {
+      console.log(Colors.FgGreen, `${log.time} | ${toTime(log.time)}`);
+      console.log(Colors.FgCyan, `name: ${log.name}`);
+
+      log.msg ? console.log(Colors.Reset, `message: ${log.msg}`) : "";
+
+      log.isTrusted
+        ? console.log(Colors.Reset, `isTrusted: ${log.isTrusted}`)
+        : "";
+
+      if (log.name === "router") {
+        console.log(
+          "\x1b[0m",
+          `title: ${log.url?.state?.title}, url: ${log.url?.path}`
+        );
       }
 
-      return message;
-    })
-      .filter(Boolean)
-      .forEach((log) => {
-        console.log(Colors.FgGreen, `${log.time} | ${toTime(log.time)}`);
-        console.log(Colors.FgCyan, `name: ${log.name}`);
+      log.mute !== undefined
+        ? console.log(Colors.FgCyan, `mute: ${log.mute}`)
+        : "";
 
-        log.msg ? console.log(Colors.Reset, `message: ${log.msg}`) : "";
+      if (log.event?.type) {
+        console.log(Colors.FgYellow, `type: ${log.event.type}`);
 
-        log.isTrusted
-          ? console.log(Colors.Reset, `isTrusted: ${log.isTrusted}`)
-          : "";
-
-        if (log.name === "router") {
-          console.log(
-            "\x1b[0m",
-            `title: ${log.url?.state?.title}, url: ${log.url?.path}`
-          );
+        if (log.event?.type === "media") {
+          console.log(Colors.FgCyan, `audio: ${log.event.audio}`);
         }
 
-        log.mute !== undefined
-          ? console.log(Colors.FgCyan, `mute: ${log.mute}`)
-          : "";
-
-        if (log.event?.type) {
-          console.log(Colors.FgYellow, `type: ${log.event.type}`);
-
-          if (log.event?.type === "media") {
-            console.log(Colors.FgCyan, `audio: ${log.event.audio}`);
-          }
-
-          if (log.event?.type === "member") {
-            console.log(Colors.FgCyan, `can: ${JSON.stringify(log.event.can)}`);
-          }
+        if (log.event?.type === "member") {
+          console.log(Colors.FgCyan, `can: ${JSON.stringify(log.event.can)}`);
         }
+      }
 
-        console.log("");
-      });
+      console.log("");
+    });
   });
 });
